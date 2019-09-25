cd98671221d2200467c8a42be678a23f95e80a68
Adam Asher <asheradamr@gmail.com>
2019-09-25 09:35:13 -0500

    output fix

--- Getuserinfo/Getuserinfo/Program.cs
+++ Getuserinfo/Getuserinfo/Program.cs
@@ -16,8 +16,10 @@ namespace Getuserinfo
             you.BirthMonth = you.Answerquestions("What month were you born?");
             you.Birthdate = you.AnswerIntQuestion("What day were you born?");
             you.Birthyear = you.AnswerIntQuestion("What year were you born?");
-            Console.WriteLine($"Hello! {you.First} {you.Last} nice to meet you. I heard these are some of your favorite things in life {you.Animal} {you.Color} {you.Book}" +
-                $"And that you were born on {you.BirthMonth} {you.Birthdate} {you.Birthyear}");
+            Console.WriteLine($"Hello! {you.First} {you.Last} nice to meet you. I heard from my little buddy at NSA these are some of your favorite things in life. Your favorite animal is a {you.Animal}, you find the color {you.Color} comforting and your favorite thing to read is {you.Book} " +
+                $"you were also born on {you.BirthMonth} {you.Birthdate} {you.Birthyear}");
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
 
 
 
